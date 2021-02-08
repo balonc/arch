3dc3364a0d29b65db5bded9b448c8fb876f74f10
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2021-02-08 23:20:43 +0100

    Actualización de código secundario root2

--- root2.sh
+++ root2.sh
@@ -5,6 +5,15 @@ BLUE="\033[0;34m"
 GREEN="\033[0;32m"
 NC='\033[0m' # No Color
 
+keys=es
+chr=chroot.sh
+pckgs=pckgs
+boot=/dev/nvme0n1p1
+root=/dev/nvme0n1p2
+home=/dev/nvme0n1p3
+swap=/dev/nvme0n1p4
+uefi=true
+
 log() {
     respuesta=$1
     msg=$2
@@ -21,7 +30,7 @@ log() {
         fi
     fi
 
-    echo -e "$time ${color}[$respuesta]${NC} $msg"
+    echo -e "$time $msg... ${color}[$respuesta]${NC}"
     # if [  $respuesta = "error"  ]; then
     #     exit 1
     # fi
@@ -38,15 +47,94 @@ checkInternet() {
 checkDir() {
     local dir=${1:?Debe proveer un argumento.}
     
-    if [[ -f "$dir" ]]; then
-        echo 1
+    if [[ -d "$dir" ]]; then
+        echo 1;
+    else
+        echo 0;
     fi
-
-    echo 0
 }
 
-log $(checkDir /etc/passwd2) Checkeando directorio
-log $(checkInternet) Checkeando interntet
-log $(checkDir /etc/passwd) Checkeando directorio
+checkDirInv() {
+    local dir=${1:?Debe proveer un argumento.}
+    
+    if [[ -d "$dir" ]]; then
+        echo 0;
+    else
+        echo 1;
+    fi
+}
 
-6 7 
+checkFile() {
+    local dir=${1:?Debe proveer un argumento.}
+    
+    if [[ -f "$dir" ]]; then
+        echo 1;
+    else
+        echo 0;
+    fi
+}
+
+checkFileInv() {
+    local dir=${1:?Debe proveer un argumento.}
+    
+    if [[ -f "$dir" ]]; then
+        echo 0;
+    else
+        echo 1;
+    fi
+}
+
+adminDiscos() {
+    if [  $uefi = true ]; then
+        mkfs.vfat -F32 $boot &> /dev/null
+        log 1 "$boot BOOT formateado en modo UEFI "
+    else
+        mkfs.ext2 $boot &> /dev/null
+        log 1 "$boot BOOT formateado en modo BIOS "
+    fi
+    mkfs.ext4 $root &> /dev/null
+    log 1 "$root ROOT formateado"
+    mkfs.ext4 $home &> /dev/null
+    log 1 "$home HOME formateado"
+    mkswap $swap &> /dev/null
+    swapon $swap &> /dev/null
+    log 1 "$swap SWAP configurado"
+    mount $root /mnt &> /dev/null
+    log 1 "$root ROOT montado en /mnt"
+    if [  $uefi = true ]; then
+        mkdir -p /mnt/boot/efi &> /dev/null
+        mount $boot /mnt/boot/efi &> /dev/null
+        log 1 "$boot BOOT montado en /mnt/boot/efi (UEFI)"
+    else
+        mkdir /mnt/boot &> /dev/null
+        mount $boot /mnt/boot &> /dev/null
+        log 1 "$boot BOOT montado en /mnt/boot (BIOS)"
+    fi
+    mkdir /mnt/home &> /dev/null
+    mount $home /mnt/home &> /dev/null
+    log 1 "$home HOME montado en /mnt/home"
+}
+
+instalacionBase() {
+    log 1 "Inicialización e instalación de sistema base"
+    pacstrap /mnt base base-devel grub ntfs-3g networkmanager efibootmgr gvfs gvfs-mtp xdg-user-dirs nano wpa_supplicant dialog xf86-input-synaptics linux linux-firmware dhcpcd &> /dev/null
+    if [  $uefi = true ]; then
+        pacstrap /mnt efibootmgr &> /dev/null
+    fi
+    genfstab -U -p /mnt >> /mnt/etc/fstab &> /dev/null
+}
+
+jaulaChroot() {
+    cp $chr /mnt &> /dev/null
+    cp $pckgs /mnt &> /dev/null
+    chmod +x /mnt/$chr &> /dev/null
+    arch-chroot /mnt ./$chr &> /dev/null
+    umount /mnt/boot &> /dev/null
+    umount /mnt/home &> /dev/null
+    umount /mnt &> /dev/null
+}
+
+adminDiscos
+log $(heckInternet) Checkeando internet
+instalacionBase
+jaulaChroot
